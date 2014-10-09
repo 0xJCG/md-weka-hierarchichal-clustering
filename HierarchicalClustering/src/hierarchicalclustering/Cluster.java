26e64a0ae3934b1068bcdeb8f97cb55f25433803
byJonyB <byJonyB@GHOST>
2014-10-09 22:40:07 +0200

    Pequeño avance. Mañana más.

--- HierarchicalClustering/src/hierarchicalclustering/Cluster.java
+++ HierarchicalClustering/src/hierarchicalclustering/Cluster.java
@@ -39,4 +39,17 @@ public class Cluster {
 	public void addInstance(Instance instance) {
 		this.instances.add(instance);
 	}
+	
+	public double distanceWithAnotherCluster(Cluster cluster) {
+		double minDistance = 1.0 / 0.0, auxDistance = 0.0;
+		for (int i = 0; i < this.instances.numInstances(); i++) {
+			for (int j = 0; j < cluster.getInstances().numInstances(); j++) {
+				auxDistance = Math.abs(this.instances.instance(i).weight() - cluster.getInstances().instance(j).weight());
+				if (auxDistance < minDistance)
+					minDistance = auxDistance;
+				auxDistance = 0.0;
+			}
+		}
+		return minDistance;
+	}
 }
--- HierarchicalClustering/src/hierarchicalclustering/Preprocess.java
+++ HierarchicalClustering/src/hierarchicalclustering/Preprocess.java
@@ -13,11 +13,17 @@ public class Preprocess {
 		return miPreprocess;
 	}
 
+	/**
+	 * Función que elimina atributos que no nos sirven para esta tarea: strings y clase.
+	 * @param instances: instancias de las que se desea eliminar atributos.
+	 * @return el conjunto de instancias sin estos atributos "inútiles".
+	 */
 	public Instances deleteUselessAtributes(Instances instances) {
-		return instances;
-	}
-	
-	public Instances deleteClass(Instances instances) {
+		if (instances.checkForStringAttributes())
+			instances.deleteStringAttributes();
+		int classPosition = instances.classIndex();
+		if (classPosition >= 0)
+			instances.deleteAttributeAt(classPosition);
 		return instances;
 	}
 }
