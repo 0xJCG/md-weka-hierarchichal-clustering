9ce03998d513a823ff08fb56d1e5651699c7d4bb
yuriy1012 <yuriy1012@gmail.com>
2014-10-17 12:45:27 +0200

    Actualizado top-down

--- HierarchicalClustering/src/hierarchicalclustering/HierarchicalClustering.java
+++ HierarchicalClustering/src/hierarchicalclustering/HierarchicalClustering.java
@@ -63,6 +63,50 @@ public class HierarchicalClustering {
 		//ClusterTree.getClusterTree().print();
 		return resultado;
 	}
+	/**
+	 * Realiza el algoritmo top-down de clustering jerarquico
+	 * @return
+	 */
+	public String run2(){
+		Instance instance1, instance2, mininstance;
+		Cluster c = new Cluster();
+		Cluster caux;
+		double minDistance, daux  = 0;
+		int i1 = 0, numIteracionesQueFaltan = this.instances.numInstances();
+		int iteraciones = numIteracionesQueFaltan;
+		
+		System.out.println("Iteracion 1 de " + iteraciones + ".");
+		ClusterList updatingClusterList = this.beginClustering2();
+		c = updatingClusterList.get(0);
+		String resultado = "A distancia 0, " + updatingClusterList.toString() + "\n";
+		while (numIteracionesQueFaltan > 1) { // Sabemos que empezamos con un único cluster y acabaremos con el número de instancias.
+			System.out.println("Iteracion " + (iteraciones - numIteracionesQueFaltan + 2) + " de " + iteraciones + ".");
+			
+			minDistance = 1.0/0.0; // Infinito.
+			for (int i = 0; i < updatingClusterList.size(); i++) {
+				double dac=0;
+				for(int j = 1;j < updatingClusterList.size(); j++){
+					instance1=c.get(i);
+					instance2=c.get(j);
+					daux=Distance.getMiDistance().getDistance().distance(instance1, instance2);
+					dac=dac+daux;
+				}
+				if(dac<minDistance){
+					minDistance=dac;
+					i1=i;
+				}
+				
+			}
+			updatingClusterList.remove(c);
+			mininstance=c.get(i1);
+			c.removeInstance(mininstance);
+			caux= new Cluster(mininstance);
+			updatingClusterList.add(c);
+			updatingClusterList.add(caux);
+			resultado += "A distancia " + minDistance + ", " + updatingClusterList.toString() + "\n";
+		}
+		return resultado;
+	}
 	
 	/**
 	 * Realiza la primera iteracion del metodo bottom-up de cluster jerarquico, es decir, una instancia por cluster.
@@ -76,6 +120,19 @@ public class HierarchicalClustering {
 			firstClusterList.add(newCluster);
 		}
 		//ClusterTree.getClusterTree().add(0f, firstClusterList);
+		return firstClusterList;
+	}
+	/**
+	 * Realiza la primera iteracion del metodo top-down de cluster jerarquico, es decir, todas las instancias en un cluster.
+	 * @return Devuelve la lista con un solo cluster que tiene todas las instancias.
+	 */
+	private ClusterList beginClustering2(){
+		Cluster newCluster= new Cluster();
+		ClusterList firstClusterList = new ClusterList();
+		for (int i=0;i<this.instances.numInstances();i++){
+			newCluster.addInstance(this.instances.instance(i));
+		}
+		firstClusterList.add(newCluster);
 		return firstClusterList;
 	}
 }
