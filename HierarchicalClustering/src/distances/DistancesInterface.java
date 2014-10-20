8a1302dd20ec5a5f606aaa12a2d0292aef369c60
yuriy1012 <yuriy1012@gmail.com>
2014-10-20 13:24:42 +0200

    Update DistancesInterface.java

--- HierarchicalClustering/src/distances/DistancesInterface.java
+++ HierarchicalClustering/src/distances/DistancesInterface.java
@@ -3,5 +3,11 @@ package distances;
 import weka.core.Instance;
 
 public interface DistancesInterface {
+	/**
+	 * Calula la distancia entre 2 instancias.
+	 * @param instance1
+	 * @param instance2
+	 * @return distance
+	 */
 	public double distance(Instance instance1, Instance instance2);
 }
