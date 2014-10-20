311a2c16241147cf8022c8a61b01ac27cc584415
yuriy1012 <yuriy1012@gmail.com>
2014-10-20 13:26:35 +0200

    Update MinkowskiDistance.java

--- HierarchicalClustering/src/distances/MinkowskiDistance.java
+++ HierarchicalClustering/src/distances/MinkowskiDistance.java
@@ -5,10 +5,20 @@ import weka.core.Instance;
 public class MinkowskiDistance implements DistancesInterface {
 	private int k;
 	
+	/**
+	 * Cosntructora de la clase.
+	 * @param k: parametro que se utiliza para obtener la distancia de Minkowski.
+	 */
 	public MinkowskiDistance(int k) {
 		this.k = k;
 	}
 	
+	/**
+	 * Calcula la distancia de Minkowski entre 2 instancias.
+	 * @param instance1
+	 * @param instance2
+	 * @return distancela: la distancia obtenida.
+	 */
 	public double distance(Instance instance1, Instance instance2) {
 		double dist = 0;
 
