609be1850e19ef4bdc3e4499251f2e0a8596d8c6
yuriy1012 <yuriy1012@gmail.com>
2014-10-20 13:33:32 +0200

    Update ClusterTree.java

--- HierarchicalClustering/src/hierarchicalclustering/ClusterTree.java
+++ HierarchicalClustering/src/hierarchicalclustering/ClusterTree.java
@@ -3,30 +3,55 @@ package hierarchicalclustering;
 public class ClusterTree {
 	private ClusterNode root;
 	private ClusterNode lastNode;
- 
+	
+	/**
+	 * Constructora de la clase
+	 * @param distance: distancia del cluster principal.
+	 * @param cluster: cluster que va a contener el nodo.
+	 */
     public ClusterTree(double distance, Cluster cluster) {
         this.root = new ClusterNode(distance, cluster);
         this.lastNode = root;
     }
- 
+    /**
+     * Constructora de la clase
+     * @param root el nodo raiz.
+     */
     public ClusterTree(ClusterNode root) {
         this.root = root;
         this.lastNode = root;
     }
 
+    /**
+     * Devuelve el nodo raiz. 
+     * @return el nodo raiz.
+     */
     public ClusterNode getRoot() {
 		return this.root;
 	}
 
+    /**
+     * Devuelve el ultimo nodo.
+     * @return el ultimo nodo.
+     */
 	public ClusterNode getLastNode() {
 		return this.lastNode;
 	}
+	/**
+	 * Añade un nodo al arbol.
+	 * @param clusterNode el nodo a añadir.
+	 */
 
 	public void addClusterNode(ClusterNode clusterNode) {
         this.addClusterNode(clusterNode, this.root);
         this.lastNode = clusterNode;
     }
     
+	/**
+	 * Añade un nodo al arbol.
+	 * @param distance: distancia del cluster principal del nodo que vamos a añadir.
+	 * @param cluster: el cluster que va a contener el nodo que vamos a añadir.
+	 */
     public void addClusterNode(double distance, Cluster cluster) {
         ClusterNode clusterNode = new ClusterNode(distance, cluster);
     	this.addClusterNode(clusterNode, this.root);
@@ -45,10 +70,17 @@ public class ClusterTree {
         }
     }
     
+    /**
+     * Convierte los datos del arbol en un String.
+     */
     public String toString() {
         return this.toString(this.root);
     }
-    
+    /**
+     * Convierte los datos del arbol en un String.
+     * @param node el nodo raiz del arbol.
+     * @return string con los datos.
+     */
     private String toString(ClusterNode node) {
         String string = "";
     	if (node != null) {
@@ -62,9 +94,19 @@ public class ClusterTree {
     	return string;
     }
     
+    /**
+     * Dibuja el arbol.
+     * @return string con el dibujo del arbol.
+     */
     public String printTree() {
 	    return this.printTree(this.root, 0);
 	}
+    /**
+     * Dibuja el arbol.
+     * @param node: nodo raiz del arbol.
+     * @param level: el nivel.
+     * @return string con el dibujo del arbol.
+     */
     
     private String printTree(ClusterNode node, int level) {
     	String string = "";
