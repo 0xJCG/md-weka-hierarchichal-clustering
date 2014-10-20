46cf9f3effb358894fb6fa83e569ff97a38a5cb7
yuriy1012 <yuriy1012@gmail.com>
2014-10-20 13:27:42 +0200

    Update SingleLink.java

--- HierarchicalClustering/src/distances/SingleLink.java
+++ HierarchicalClustering/src/distances/SingleLink.java
@@ -4,6 +4,13 @@ import weka.core.Instance;
 import hierarchicalclustering.Cluster;
 
 public class SingleLink implements LinksInterface {
+	 /**
+	 * Pre: los clusters no deben estar vacíos.
+	 * Calcula la distancia intergrupal entre 2 clusters por single-link(la distancia menor entre los clusters).
+	 * @param cluster1
+     	 * @param cluster2
+     	 * @return distance: la distancia obtenida.
+	 */
 	public double calculateClusterDistance(Cluster cluster1, Cluster cluster2) {
 		Instance instance1, instance2;
 		double distance = 1.0/0.0, tempDist = 0;
