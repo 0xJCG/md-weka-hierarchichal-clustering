4e2969f94256b2ed07f5f8261539dc4dd48d6d2f
yuriy1012 <yuriy1012@gmail.com>
2014-10-20 13:21:50 +0200

    Update ChebyshevDistance.java

--- HierarchicalClustering/src/distances/ChebyshevDistance.java
+++ HierarchicalClustering/src/distances/ChebyshevDistance.java
@@ -3,6 +3,12 @@ package distances;
 import weka.core.Instance;
 
 public class ChebyshevDistance implements DistancesInterface {
+	/**
+	 * Calcula la distancia de Chebyshev entre 2 instancias.
+	 * @param instance1
+	 * @param instance2
+	 * @return distance: la distancia obtenida.
+	 */
 	public double distance(Instance instance1, Instance instance2) {
 		double dist = 0, aux = 0;
 
