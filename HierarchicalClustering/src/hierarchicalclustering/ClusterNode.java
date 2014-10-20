b0aaaed77cae2990c7d268864926f09ae6a76f01
yuriy1012 <yuriy1012@gmail.com>
2014-10-20 13:32:15 +0200

    Update ClusterNode.java

--- HierarchicalClustering/src/hierarchicalclustering/ClusterNode.java
+++ HierarchicalClustering/src/hierarchicalclustering/ClusterNode.java
@@ -37,26 +37,50 @@ public class ClusterNode {
 		return cluster;
 	}
 
+    /**
+     * Devuelve el nodo padre.
+     * @return Nodo padre.
+     */
 	public ClusterNode getParent() {
         return this.parent;
     }
 
+	/**
+	 * Asigna un nodo al nodo padre.
+	 * @param parent el nodo a asignar.
+	 */
     public void setParent(ClusterNode parent) {
         this.parent = parent;
     }
 
+    /**
+     * Devuelve la rama izquierda.
+     * @return el nodo raiz de la rama izquierda.
+     */
     public ClusterNode getLeftBranch() {
         return this.leftBranch;
     }
-
+    
+    /**
+     * Asigna un nodo la rama izquierda.
+     * @param leftBranch: el nodo que se le va asignar a el nodo raiz de la rama izquierda.
+     */
     public void setLeftBranch(ClusterNode leftBranch) {
         this.leftBranch = leftBranch;
     }
 
+    /**
+     * Devuelve la rama derecha.
+     * @return el nodo raiz de la rama derecha.
+     */
     public ClusterNode getRightBranch() {
         return this.rightBranch;
     }
 
+    /**
+     * Asigna un nodo la rama derecha.
+     * @param leftBranch: el nodo que se le va asignar a el nodo raiz de la rama derecha.
+     */
     public void setRightBranch(ClusterNode rightBranch) {
         this.rightBranch = rightBranch;
     }
